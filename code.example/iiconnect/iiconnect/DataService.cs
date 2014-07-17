b0f49d4aee6296f8d56df58a83fb9580478677e8
mar3 <irisawa.masaru@gmail.com>
2014-07-17 11:26:41 +0900

    wip

---
+++ code.example/iiconnect/iiconnect/DataService.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iiconnect
+{
+	sealed class DataService
+	{
+		private System.Data.IDbConnection _connection = null;
+
+		private System.Data.IDataReader _reader = null;
+
+		public DataService()
+		{
+			string connection_string = System.Configuration.ConfigurationManager.AppSettings["CONNECTION_STRING"];
+			System.Data.IDbConnection connection = new Ingres.Client.IngresConnection(connection_string);
+			connection.Open();
+			this._connection = connection;
+		}
+
+		private void AssertConnection()
+		{
+			if (this._connection == null)
+				throw new Exception("No connection.");
+		}
+
+		public System.Data.IDataReader Select(string sql)
+		{
+			this.AssertConnection();
+
+			Close(this._reader);
+			this._reader = null;
+
+			System.Data.IDbCommand command = this.CreateDefaultCommand();
+			command.CommandText = sql;
+			this._reader = command.ExecuteReader();
+			return this._reader;
+		}
+
+		public System.Data.IDataReader Select(string sql, params object[] parameters)
+		{
+			this.AssertConnection();
+
+			Close(this._reader);
+			this._reader = null;
+
+			System.Data.IDbCommand command = this.CreateDefaultCommand();
+			command.CommandText = sql;
+			PrepareParameters(command, parameters);
+			this._reader = command.ExecuteReader();
+			return this._reader;
+		}
+
+		public int Execute(string sql)
+		{
+			this.AssertConnection();
+
+			System.Data.IDbCommand command = this.CreateDefaultCommand();
+			command.CommandText = sql;
+			return command.ExecuteNonQuery();
+		}
+
+		public int Execute(string sql, params object[] parameters)
+		{
+			this.AssertConnection();
+
+			System.Data.IDbCommand command = this.CreateDefaultCommand();
+			command.CommandText = sql;
+			PrepareParameters(command, parameters);
+			return command.ExecuteNonQuery();
+		}
+
+		private static void PrepareParameters(System.Data.IDbCommand command, params object[] parameters)
+		{
+			foreach (object unknown in parameters)
+			{
+				System.Data.IDbDataParameter p = command.CreateParameter();
+				p.Value = unknown;
+				command.Parameters.Add(p);
+			}
+		}
+
+		private System.Data.IDbCommand CreateDefaultCommand()
+		{
+			System.Data.IDbCommand command = this._connection.CreateCommand();
+			command.CommandTimeout = 30;
+			command.CommandType = System.Data.CommandType.Text;
+			return command;
+		}
+
+		public void Close()
+		{
+			Close(this._reader);
+			this._reader = null;
+
+			Close(this._connection);
+			this._connection = null;
+		}
+
+		private static void Close(System.Data.IDataReader reader)
+		{
+			if (reader == null)
+				return;
+			reader.Close();
+		}
+
+		private static void Close(System.Data.IDbConnection reader)
+		{
+			if (reader == null)
+				return;
+			reader.Close();
+		}
+	}
+}
---
+++ code.example/iiconnect/iiconnect/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iiconnect
+{
+	public sealed class Program
+	{
+		private Program()
+		{
+
+		}
+
+		public static void Main(string[] args)
+		{
+			try
+			{
+				HitTest();
+				Console.WriteLine("Ok.");
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("[ERROR] {0}", e);
+			}
+		}
+
+		private static void HitTest()
+		{
+			DataService service = null;
+			try
+			{
+				service = new DataService();
+			}
+			finally
+			{
+				if (service != null)
+					service.Close();
+			}
+		}
+	}
+}
